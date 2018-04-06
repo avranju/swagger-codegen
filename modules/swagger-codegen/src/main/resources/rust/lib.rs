d7a8940cacbbdaaf8f64512b6fd45ceaea0df870
Rajasekharan Vengalil <rajave@microsoft.com>
2018-04-05 18:06:52 -0700

    Add support for sas tokens

--- modules/swagger-codegen/src/main/resources/rust/lib.rs
+++ modules/swagger-codegen/src/main/resources/rust/lib.rs
@@ -1,10 +1,12 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[macro_use]
+extern crate failure;
+extern crate futures;
 extern crate hyper;
 extern crate serde;
 extern crate serde_json;
-extern crate futures;
 extern crate url;
 
 pub mod apis;
